04261ede5c075851b36a72fb110dd3cd6b36e08a
biocro-action <bogus@url.com>
2024-03-06 17:42:49 +0000

    generated pkgdown and bookdown documentation

---
+++ v3.1.0/doxygen/doxygen_docs_modules/c3photo_8cpp.js
@@ -0,0 +1,5 @@
+var c3photo_8cpp =
+[
+    [ "c3photoC", "c3photo_8cpp.html#a996bf8237e3afc735bc21f74140352e1", null ],
+    [ "solo", "c3photo_8cpp.html#a2c2b79a3f622d45a0dc9017ef87a4f98", null ]
+];
